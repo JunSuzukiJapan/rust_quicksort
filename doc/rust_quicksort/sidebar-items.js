92731a6cd94f1c4f4e7f0002eff45fe3ff78c3fc
Jun Suzuki <jun.suzuki.japan@gmail.com>
2016-12-09 15:09:45 +0900

    add doc

---
+++ doc/rust_quicksort/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["quicksort","Sorts slice."]]});
---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["rust_quicksort"] = {"doc":"","items":[[5,"quicksort","rust_quicksort","Sorts slice.",null,null]],"paths":[]};
+initSearch(searchIndex);
