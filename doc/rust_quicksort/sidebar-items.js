374a81ca61ee9a846a6a6625e319cb36c6bc91a8
Jun Suzuki <jun.suzuki.japan@gmail.com>
2016-12-09 15:35:29 +0900

    remove doc folder

--- doc/rust_quicksort/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["quicksort","Sorts slice."]]});
--- doc/search-index.js
+++
@@ -1,3 +0,0 @@
-var searchIndex = {};
-searchIndex["rust_quicksort"] = {"doc":"","items":[[5,"quicksort","rust_quicksort","Sorts slice.",null,null]],"paths":[]};
-initSearch(searchIndex);
